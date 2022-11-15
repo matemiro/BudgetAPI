db0567f83be76251460eadac4da418750b4cf131
matemiro <mateusz.piotr.miroslawski@gmail.com>
2022-11-15 20:27:23 +0100

    Add view to register user.

--- budget_api/urls.py
+++ budget_api/urls.py
@@ -14,15 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
+from users.views import CreateUserView
+
+router = routers.DefaultRouter()
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path(
         "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
