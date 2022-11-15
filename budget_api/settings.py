43dc95a9aaff0545d888455f038e53d7d3356b17
matemiro <mateusz.piotr.miroslawski@gmail.com>
2022-11-15 22:39:31 +0100

    Change token lifetime to 15 days if DEBUG is True. Set DEBUG variable as env variable (default set to False).

--- budget_api/settings.py
+++ budget_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 from decouple import config
@@ -27,7 +27,7 @@ SECRET_KEY = config(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS").split(",")
 
@@ -140,3 +140,9 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
+
+if DEBUG:
+    SIMPLE_JWT = {
+        'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
+        'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    }
